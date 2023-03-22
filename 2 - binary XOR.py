2251af12ad28028c81f3453cff13f1533bb05dad
vliegbanaan <56888959+vliegbanaan@users.noreply.github.com>
2023-03-22 16:48:49 +0100

    Opdracht 2 added
    geen foutmeldingen, alleen de print xor_output geeft niet het resultaat wat we verwachten, Joyce vragen.

--- 2 - binary XOR.py
+++ 2 - binary XOR.py
@@ -16,10 +16,12 @@ def fixed_length_xor(text, key):
     bytes
         binary XOR of text & key
     """
-    
+
+    # XOR door elke byte heen
+    xor_output = bytes([text[x] ^ key[x] for x in range(len(text))])
     return xor_output
 
-# Laat deze asserts onaangetast!
+# Laat deze asserts onaangetast! OK IS GOED
 assert type(fixed_length_xor(b'foo',b'bar')) == bytes
 assert b64encode(fixed_length_xor(b'foo',b'bar')) == b'BA4d'
 
@@ -39,10 +41,12 @@ def repeating_key_xor(text, key):
     bytes
         binary XOR of text & key
     """
-
+    key = key * (len(text) // len(key)) + key[:len(text) % len(key)]
+    xor_output = bytes([text[x] ^ key[x] for x in range(len(text))])
+    print(xor_output)
     return xor_output
 
-# Laat deze asserts onaangetast!
+# Laat deze asserts onaangetast! OK IS GOED
 assert type(repeating_key_xor(b'all too many words',b'bar')) == bytes
 assert b64encode(repeating_key_xor(b'all too many words',b'bar'))\
    == b'Aw0eQhUdDUEfAw8LQhYdEAUB'
