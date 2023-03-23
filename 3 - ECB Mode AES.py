76edc8387deb2b9dad0cd5c2ee4cc9abdeaededb
vliegbanaan <56888959+vliegbanaan@users.noreply.github.com>
2023-03-23 12:27:16 +0100

    Opdracht 3 added
    - Eerste print voor test om te kijken of de cypher text word gezien en de key.
    - Bestasnd wordt gevonden en naar onze weet decrypted
    - #ToDo decrypted bestand wordt niet geprint, we weten niet waarom

--- 3 - ECB Mode AES.py
+++ 3 - ECB Mode AES.py
@@ -1,4 +1,5 @@
 from base64 import b64decode
+from Crypto.Cipher import AES
 
 def ECB_decrypt(ciphertext, key):
     """Accepts a ciphertext in byte-form,
@@ -18,11 +19,24 @@ def ECB_decrypt(ciphertext, key):
         decrypted plaintext
     """
 
+    print('Decrypting ciphertext:', ciphertext)
+    print('Deze key wordt:', key)
+
+    cipher = AES.new(key, AES.MODE_ECB)
+    plaintext = cipher.decrypt(ciphertext)
     return plaintext
 
+    with open('file3.txt', 'rb') as f:  # Open bestand en ga door content heen. RB gebruiken we om het bestand in binary mode te lezen.
+     ciphertext = f.read()
+
+    key = b'SECRETSAREHIDDEN'
+    
+    plaintext = ECB_decrypt(ciphertext, key)   
+    print(plaintext.decode('utf-8')) # Print decrypted bytes to text, hiervoor gebruiken we char encoding zoals utf-8.
 
 # Laat deze asserts onaangetast & onderaan je code!
 ciphertext = b64decode('86ueC+xlCMwpjrosuZ+pKCPWXgOeNJqL0VI3qB59SSY=')
 key = b'SECRETSAREHIDDEN'
 assert ECB_decrypt(ciphertext, key)[:28] == \
     b64decode('SGFzdCBkdSBldHdhcyBaZWl0IGZ1ciBtaWNoPw==')
+
